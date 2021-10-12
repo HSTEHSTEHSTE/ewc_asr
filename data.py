f2251d3a459eaf6cbc8ad8a22d683c45998f85c8
Xinyuan Li <xli257@b19.clsp.jhu.edu>
2021-10-12 16:14:35 -0400

    Bug fixes and suppress warnings

--- data.py
+++ data.py
@@ -9,21 +9,19 @@ class PermutedMNIST(datasets.MNIST):
         super(PermutedMNIST, self).__init__(root, train, download=True)
         assert len(permute_idx) == 28 * 28
         if self.train:
-            self.train_data = torch.stack([img.float().view(-1)[permute_idx] / 255
-                                           for img in self.train_data])
+            self.training_data = torch.stack([img.float().view(-1)[permute_idx] / 255 for img in self.data])
         else:
-            self.test_data = torch.stack([img.float().view(-1)[permute_idx] / 255
-                                          for img in self.test_data])
+            self.testing_data = torch.stack([img.float().view(-1)[permute_idx] / 255 for img in self.data])
 
     def __getitem__(self, index):
 
         if self.train:
-            img, target = self.train_data[index], self.train_labels[index]
+            img, target = self.training_data[index], self.targets[index]
         else:
-            img, target = self.test_data[index], self.test_labels[index]
+            img, target = self.testing_data[index], self.targets[index]
 
         return img, target
 
     def get_sample(self, sample_size):
         sample_idx = random.sample(range(len(self)), sample_size)
-        return [img for img in self.train_data[sample_idx]]
+        return [img for img in self.training_data[sample_idx]]
